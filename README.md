b1fb39448520b85948d3987506efd76bb1f83e8b
Scott Endicott <sendicott1391@gmail.com>
2016-09-05 10:28:12 -0400

    Individual letter boxes can now be created with classes and ids based on length of word

--- README.md
+++ README.md
@@ -7,7 +7,13 @@ necessity:
 1.  Acquire an array of single words
 2.  Allow user to specify a length of word they want in an input field
 3.  Remove any words that do not match this length
+
 4.  Display the appropriate number of blanks in a section
+        a. these blanks should have a background-color, but no letters at first
+        b. they should have some sort of identifier in order to be targeted
+                i. create an array as long as the length chosen
+                ii. use indexOf() to place letters once a letter must be placed
+                iii. or would id be better?
 5.  Create an input field for guessing individual letters
         a. when a letter is guessed, if there is a word in the wordList that does not have that letter, remove all words with the letter and count the guess as wrong
         b. when a letter is guessed, if all of the remaining words have that letter, pick a random word from the list, reveal the letter(s)in the appropriate blanks, and strike everything from the wordList that doesn't have that same letter in the same position
--- app.js
+++ app.js
@@ -25,6 +25,7 @@ function removeWithLetter(letter) {
     });
 }
 
+
 window.addEventListener('load', function () {
     currentList(allWords);
 
@@ -32,27 +33,37 @@ window.addEventListener('load', function () {
     let guessBtn = document.querySelector('#guess-click');
 
     lengthBtn.addEventListener('click', function () {
+        // can I lift this variable up a scope so it can be accessed later?
         let targetLength = parseInt(document.querySelector('#word-length').value);
         console.log("Length of word must be: " + targetLength + " letters");
         allWords = allWords.filter(function (word) {
             return (word.length === targetLength);
         });
+        // would this be where to put an array of blanks, to be filled
+        // with letters later? Could it be accessed?
+        let letterBoxRow = document.querySelector("#letterBoxRow");
+        // let letterArray = [];
+        for (let i = 0; i < targetLength; i++) {
+            console.log("loop runs " + i + " times");
+            let singleBox = document.createElement("div");
+            singleBox.classList.add("#boxStyling");
+            singleBox.setAttribute("id", 'letterBox' + (i + 1));
+            letterBoxRow.appendChild(singleBox);
+        }
         currentList(allWords);
     });
 
     guessBtn.addEventListener('click', function () {
         let letterGuess = document.querySelector('#guess-box').value;
-        console.log(typeof(letterGuess));
         console.log("The word must contain the letter: " + letterGuess);
         if (removeWithLetter(letterGuess).length > 0) {
             console.log("Nope! That letter definitely isn't in the word.");
-            allWords = allWords.filter(removeWithLetter(letterGuess));
-                // This is running, it is recognizing that there are words left without that letter,
-                // but it is filtering out all of the words and leaving nothing left
-                // removeWithLetter(letterGuess);
+            allWords = removeWithLetter(letterGuess);
             currentList(allWords);
         } else {
-            console.log("Well even a blind squirrel finds a nut once in a while.");
+            console.log("Got a letter right");
+            currentList(allWords);
+
         }
     });
 });
